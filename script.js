89289488a51ab00c42f388a49569aeaa634a8102
lspyq <lspengyouquan@163.com>
2017-08-07 08:04:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ var x4 = "http://api.baiyug.cn/vip/index.php?url=";
 var x5 = "http://jiexi.92fz.cn/player/vip.php?url=";
 var xlx = x1;
 if(navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)) {
-	document.getElementById("sp").style.height = "13em";
-	document.getElementById("sp").style.paddingbottom = "";
+	document.getElementById("sp").style.height = "40%";
 } else {
 	document.getElementById("sp").style.height = "100%";
 }
