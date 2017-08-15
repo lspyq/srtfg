d507d51877e6c11168fd5de99750455ccf5cdf81
lspyq <lspengyouquan@163.com>
2017-08-15 21:59:13 +0800

    Update and rename vip.html to spk.js

---
+++ spk.js
@@ -0,0 +1,9 @@
+var spk = [
+'http://v.youku.com/v_show/id_XMTgzNDM2ODA1Ng==.html',//鹬Piper [2016]
+'http://v.youku.com/v_show/id_XMjc4MzE3MTU3Mg==.html',//《变形金刚5:最后的骑士》内地定档6.23 新预告战斗一刻不停
+'http://v.youku.com/v_show/id_XMjc4NTQ3NDk0OA==.html',//《绝世高手》“江浙沪包邮”版预告 卢正雨郭采洁爆笑祖国
+'http://v.youku.com/v_show/id_XMjczMDY2NjQyMA==.html',//《战狼2》吴刚腰伤不下火线 单手换弹夹又圈粉
+'http://v.youku.com/v_show/id_XMjgyMDQ3MjIyNA==.html',//《蜘蛛侠:英雄归来》哭！小蜘蛛竟被钢铁侠放鸽子
+'http://v.youku.com/v_show/id_XMjc4MTAzOTU5Ng==.html',//《新木乃伊》“真实动作”特辑
+'http://v.youku.com/v_show/id_XMjgxMTIxNDI4OA==.html',//“国民初恋”克里斯·派恩魅力登场 被赞是天赐的礼物
+];
