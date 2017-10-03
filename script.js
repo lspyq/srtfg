e403e5ae0b25b52f6c5ee6b9017e42f1ea9bee6a
lspyq <lspengyouquan@163.com>
2017-10-03 18:42:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	var x5 = "http://jiexi.92fz.cn/player/vip.php?url=";
 	var xlx;
 	var dz;
-	console.log(bds)；
+	console.log(bds);
 $('#fx').popover('open')
 	function GetQueryString(name) {
 		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
