de9a5aad1d495818fc96d863a067634ec99b2271
lspyq <lspengyouquan@163.com>
2019-10-04 19:41:56 +0800

    Create versionUp.js

---
+++ cb/versionUp.js
@@ -0,0 +1,8 @@
+import queryString from 'http://lspyq.cn/cb/query-string.js';
+export default {
+	newVersion: "1.0.1",
+	// queryStringUrl:"./common/query-string.js",
+	queryStringUrl: "./common/query-string.js",
+	queryString: queryString
+
+}
