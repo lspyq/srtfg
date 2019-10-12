94efee4154901de80663574ff1af407227c75e3c
lspyq <lspengyouquan@163.com>
2019-10-12 14:42:18 +0800

    Create index.js

---
+++ hb/index.js
@@ -0,0 +1,15 @@
+var index = `
+<div id="app">
+        <div class="dh">
+            <ul></ul>
+        </div>
+        <div class="nr">
+            <ul></ul>
+        </div>
+        <div class="fy">
+            <ul></ul>
+
+            <div class="tz"></div>
+        </div>
+    </div>
+`
